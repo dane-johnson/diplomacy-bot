d1c8ac7ca60a239566a87fe183b6f2819fafc406
Cloud User <ec2-user@ip-172-31-34-80.us-east-2.compute.internal>
2019-02-22 18:38:44 +0000

    Pass verification

--- src/server.py
+++ src/server.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
   return "Hello World"
+
+@app.route("/event", methods=['POST'])
+def returnRequestChallenge():
+	print request.get_json()
+	return request.get_json()['challenge']
 
 if __name__ == '__main__':
   app.run('0.0.0.0', port=80)
