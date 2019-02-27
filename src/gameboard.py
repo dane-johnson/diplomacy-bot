8e927875e92c464b315ecf31b5b6c3eaac54331a
Dane Johnson <dane.johnson@monsanto.com>
2019-02-27 15:06:43 -0600

    Began adding graph representing the game map

---
+++ src/gameboard.py
@@ -0,0 +1,8 @@
+gameboard = {
+  'nao': {'type': 'water', 'borders': ['nwg', 'cly', 'lvp', 'iri', 'mao']},
+  'nwg': {'type': 'water', 'borders': ['bar', 'nwy', 'nth', 'edi', 'cly' 'nao']},
+  'bar': {'type': 'water', 'borders': ['stp', 'nwy', 'nwg']},
+  'stp': {'type': 'coastal', 'supply': 'russia', 'borders': ['bar', 'mos', 'lvn', 'bot', 'fin', 'nwy']},
+  'mos': {'type': 'land', 'supply': 'russia', 'borders': ['stp', 'sev', 'ukr', 'war', 'lvn']},
+  'sev': {'type': 'coastal', 'supply': 'russia', 'border': ['mos', 'arm', 'bla', 'rum', 'ukr']},
+}
