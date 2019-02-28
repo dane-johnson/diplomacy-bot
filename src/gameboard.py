f31ede36d08e5271388b9a1af16453ecf07e08ba
Dane Johnson <daneallenjohnson@protonmail.com>
2019-02-27 20:02:10 -0600

    More gameboard, added function to print issues with the gameboard

--- src/gameboard.py
+++ src/gameboard.py
@@ -1,6 +1,8 @@
+import re
+
 gameboard = {
   'nao': {'type': 'water', 'borders': ['nwg', 'cly', 'lvp', 'iri', 'mao']},
-  'nwg': {'type': 'water', 'borders': ['bar', 'nwy', 'nth', 'edi', 'cly' 'nao']},
+  'nwg': {'type': 'water', 'borders': ['bar', 'nwy', 'nth', 'edi', 'cly', 'nao']},
   'bar': {'type': 'water', 'borders': ['stp', 'nwy', 'nwg']},
   'stp': {'type': 'coastal', 'supply': 'russia', 'borders': ['bar', 'mos', 'lvn', 'bot', 'fin', 'nwy']},
   'mos': {'type': 'land', 'supply': 'russia', 'borders': ['stp', 'sev', 'ukr', 'war', 'lvn']},
@@ -11,6 +13,27 @@ gameboard = {
   'ion': {'type': 'water', 'borders': ['adr', 'alb', 'gre', 'aeg', 'eas', 'tun', 'tys', 'nap', 'apu']},
   'tun': {'type': 'coastal', 'supply': 'neutral', 'borders': ['tys', 'ion', 'naf', 'wes']},
   'naf': {'type': 'coastal', 'supply': 'none', 'borders': ['wes', 'tun', 'mao']},
-  'mao': {'type': 'water', 'borders': ['nao', 'iri', 'eng', 'bre', 'gas', 'spa', 'por']},
+  'mao': {'type': 'water', 'borders': ['nao', 'iri', 'eng', 'bre', 'gas', 'spa', 'por', 'wes', 'naf']},
   'iri': {'type': 'water', 'borders': ['nao', 'lvp', 'wal', 'eng', 'mao']},
+  'lvp': {'type': 'coastal', 'supply': 'england', 'borders': ['cly', 'edi', 'yor', 'wal', 'iri', 'nao']},
+  'cly': {'type': 'coastal', 'supply': 'none', 'borders': ['nao', 'nwg', 'edi', 'lvp']},
+  'edi': {'type': 'coastal', 'supply': 'england', 'borders': ['nwg', 'nth', 'yor', 'lvp', 'cly']},
+  'nth': {'type': 'water', 'borders': ['nwg', 'nwy', 'ska', 'den', 'hel', 'hoi', 'bel', 'eng', 'lon', 'yor', 'edi']},
+  'nwy': {'type': 'coastal', 'supply': 'neutral', 'borders': ['nwg', 'bar', 'stp', 'fin', 'swe', 'ska', 'nth']},
+  'swe': {'type': 'coastal', 'supply': 'neutral', 'borders': ['nwy', 'fin', 'bot', 'bal', 'ska']},
+  'fin': {'type': 'coastal', 'supply': 'none', 'borders': ['nwy', 'stp', 'bot', 'swe']},
+  'bot': {'type': 'water', 'borders': ['swe', 'fin', 'stp', 'lvn', 'bal']},
 }
+
+def print_board_issues():
+  for territory in gameboard:
+    if not re.match(r"[a-z]{3}", territory):
+      print "%s isn't a valid territory name" % territory
+    for border in gameboard[territory]['borders']:
+      if border not in gameboard:
+        print "%s borders %s but %s isn't on the gameboard" % (territory, border, border)
+      elif territory not in frozenset(gameboard[border]['borders']):
+        print "%s borders %s but %s does not border %s" % (territory, border, border, territory)
+
+if __name__ == "__main__":
+  print_board_issues()
