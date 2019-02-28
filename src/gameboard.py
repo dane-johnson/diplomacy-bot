0755918d5c7b9c8f2a0d328909b229fc662a4301
Dane Johnson <dane.johnson@monsanto.com>
2019-02-28 11:24:39 -0600

    More gameboard

--- src/gameboard.py
+++ src/gameboard.py
@@ -18,11 +18,27 @@ gameboard = {
   'lvp': {'type': 'coastal', 'supply': 'england', 'borders': ['cly', 'edi', 'yor', 'wal', 'iri', 'nao']},
   'cly': {'type': 'coastal', 'supply': 'none', 'borders': ['nao', 'nwg', 'edi', 'lvp']},
   'edi': {'type': 'coastal', 'supply': 'england', 'borders': ['nwg', 'nth', 'yor', 'lvp', 'cly']},
-  'nth': {'type': 'water', 'borders': ['nwg', 'nwy', 'ska', 'den', 'hel', 'hoi', 'bel', 'eng', 'lon', 'yor', 'edi']},
+  'nth': {'type': 'water', 'borders': ['nwg', 'nwy', 'ska', 'den', 'hel', 'hol', 'bel', 'eng', 'lon', 'yor', 'edi']},
   'nwy': {'type': 'coastal', 'supply': 'neutral', 'borders': ['nwg', 'bar', 'stp', 'fin', 'swe', 'ska', 'nth']},
   'swe': {'type': 'coastal', 'supply': 'neutral', 'borders': ['nwy', 'fin', 'bot', 'bal', 'ska']},
   'fin': {'type': 'coastal', 'supply': 'none', 'borders': ['nwy', 'stp', 'bot', 'swe']},
   'bot': {'type': 'water', 'borders': ['swe', 'fin', 'stp', 'lvn', 'bal']},
+  'lvn': {'type': 'coastal', 'supply': 'none', 'borders': ['bot', 'stp', 'mos', 'war', 'pru', 'bal']},
+  'ukr': {'type': 'land', 'supply': 'none', 'borders': ['mos', 'sev', 'rum', 'gal', 'war']},
+  'bla': {'type': 'water', 'borders': ['sev', 'arm', 'ank', 'con', 'bul', 'aeg', 'bul']},
+  'ank': {'type': 'coastal', 'supply': 'turkey', 'borders': ['bla', 'arm', 'smy', 'con']},
+  'smy': {'type': 'coastal', 'supply': 'turkey', 'borders': ['ank', 'arm', 'syr', 'eas', 'aeg', 'con']},
+  'aeg': {'type': 'water', 'borders': ['bul', 'con', 'bla', 'smy', 'eas', 'ion', 'gre']},
+  'gre': {'type': 'coastal', 'supply': 'neutral', 'borders': ['ser', 'bul', 'aeg', 'ion', 'adr', 'alb']},
+  'nap': {'type': 'coastal', 'supply': 'italy', 'borders': ['apu', 'ion', 'tys', 'rom', 'ven']},
+  'tys': {'type': 'water', 'borders': ['lyo', 'tus', 'rom', 'nap' , 'ion', 'tun', 'wes']},
+  'wes': {'type': 'water', 'borders': ['lyo', 'tys', 'tun', 'naf', 'mao', 'spa']},
+  'spa': {'type': 'coastal', 'supply': 'neutral', 'borders': ['mao', 'gas', 'mar', 'lyo', 'wes', 'por']},
+  'por': {'type': 'coastal', 'supply': 'neutral', 'borders': ['mao', 'spa']},
+  'gas': {'type': 'coastal', 'supply': 'none', 'borders': ['bre', 'par', 'bur', 'mar', 'spa', 'mao']},
+  'bre': {'type': 'coastal', 'supply': 'france', 'borders': ['eng', 'pic', 'par', 'gas', 'mao']},
+  'eng': {'type': 'water', 'borders': ['wal', 'lon', 'nth', 'bel', 'pic', 'bre', 'mao', 'iri']},
+  'wal': {'type': 'coastal', 'supply': 'none', 'borders': ['lvp', 'yor', 'lon', 'eng', 'iri']}
 }
 
 def print_board_issues():
