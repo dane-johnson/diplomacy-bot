27be741bd81df571c4c79a40026e5efc4ab07394
Dane Johnson <dane.johnson@monsanto.com>
2019-03-12 10:53:12 -0500

    Can add a fleet as well

--- src/image.py
+++ src/image.py
@@ -1,34 +1,39 @@
 from PIL import Image
 
 ARMY_SIZE = (20, 15)
+FLEET_SIZE = (20, 15)
 
 GREEN = (0, 255, 0)
 BLUE = (0, 102, 255)
 
 _gameboard = Image.open('resources/gameboard.png')
 _army = Image.open('resources/cannon.gif')
+_fleet = Image.open('resources/boat.gif')
 
 def get_gameboard():
   return _gameboard.copy()
 
-def add_army(gameboard, coords, color):
-  army = get_army_color(color)
-  army_mask = army.convert('RGBA')
-  gameboard.paste(army, coords, mask=army_mask)
+def add_piece(gameboard, piece_name, coords, color):
+  piece = get_piece_color(piece_name, color)
+  piece_mask = piece.convert('RGBA')
+  gameboard.paste(piece, coords, mask=piece_mask)
 
-def get_army_color(rgb):
+def get_piece_color(piece_name, rgb):
   r, g, b = rgb
-  army = _army.resize(ARMY_SIZE).convert('RGBA')
-  pixels = army.load()
-  for y in xrange(army.size[1]):
-    for x in xrange(army.size[0]):
+  if piece_name == 'army':
+    piece = _army.resize(ARMY_SIZE).convert('RGBA')
+  else:
+    piece = _fleet.resize(FLEET_SIZE).convert('RGBA')
+  pixels = piece.load()
+  for y in xrange(piece.size[1]):
+    for x in xrange(piece.size[0]):
       if pixels[x, y] == (255, 255, 255, 255):
         pixels[x, y] = (r, g, b, 255)
-  return army
+  return piece
 
 def demo():
   gameboard = get_gameboard()
-  add_army(gameboard, (40, 60), BLUE)
+  add_piece(gameboard, 'fleet', (40, 60), BLUE)
   gameboard.show()
 
 if __name__ == "__main__":
