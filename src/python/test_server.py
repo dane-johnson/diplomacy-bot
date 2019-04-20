5829b307b29f9296d8be106050e2da0e3a5b009e
Dane Johnson <dane_johnson@ymail.com>
2019-04-20 17:51:41 -0500

    Added some tests for order_error

--- src/python/test_server.py
+++ src/python/test_server.py
@@ -1,6 +1,6 @@
 import unittest
 import server
-
+import gameboard
 def create_event(type, text, user="no user", channel="no channel"):
   return {"type": type, "text": text, "user": user, "channel": channel}
 def create_message(text, user="no user", channel="no channel"):
@@ -11,6 +11,15 @@ def create_app_mention(text, user="no user"):
 class ServerTest(unittest.TestCase):
   def setUp(self):
     server.init_gamestate()
+  def tearDown(self):
+    server.gamestate = {
+      "players": {},
+      "units": {},
+      "orders": {},
+      "mode": "pregame",
+      "gameboard": gameboard.gameboard,
+      "dislodged_units": {}
+    }
   def test_parse_order(self):
     self.assertEqual(
       server.parse_order("army abc holds"),
@@ -35,6 +44,27 @@ class ServerTest(unittest.TestCase):
     self.assertEqual(server.gamestate['orders'], {"abc": hold_order})
     server.add_order(attack_order)
     self.assertEqual(server.gamestate['orders'], {"abc": attack_order})
+  def test_order_error(self):
+    server.gamestate['gameboard'] = {"abc": {"type": "land", "borders": []},
+                                     "xyz": {"type": "land", "borders": []}}
+    self.assertEqual(
+      server.order_error({"action": "hold", "territory": "bcd"}, "england"),
+      "Territory bcd is not valid"
+    )
+    self.assertEqual(
+      server.order_error({"action": "move/attack", "territory": "abc", "to": "bcd"}, "england"),
+      "Territory bcd is not valid"
+    )
+    self.assertEqual(
+      server.order_error({"action": "support", "territory": "abc", "supporting": "bcd"}, "england"),
+      "Territory bcd is not valid"
+    )
+    self.assertEqual(
+      server.order_error({"action": "convoy", "territory": "abc", "from": "bcd", "to": "xyz"}, "england"),
+      "Territory bcd is not valid"
+    )
+    
+
 
 if __name__ == "__main__":
   unittest.main()
